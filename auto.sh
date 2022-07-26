cb4473dc038139edd67b42c9f4b0c66cfb8bde12
See-Night <wangshibo142@gmail.com>
2022-07-26 22:20:33 +0800

    Docker自动部署脚本

--- auto.sh
+++ auto.sh
@@ -6,7 +6,7 @@ if [ $# -ne 0 ]; then
     if [[ $1 == "start" && $# -eq 2 ]]; then
         cat room_list.txt | while read room_id container_name
         do
-            docker run --name $container_name -v `readlink -f $2`:/app/video -d bililive $room_id
+            docker run --name $container_name -v `readlink -f $2`:/app/video -d seenight/bililive $room_id
         done
     elif [ $1 == "stop" ]; then
         cat room_list.txt | while read room_id container_name
